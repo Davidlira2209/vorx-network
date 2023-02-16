95feefa05e183dc632b4bbbab3c51edd2cc12ea3
root <root@ip-172-31-1-213.ec2.internal>
2023-02-16 00:40:42 +0000

    add: create igw-prod-vorx-vpc

--- network.tf
+++ network.tf
@@ -47,6 +47,13 @@ resource "aws_subnet" "vorx-subnet-priv-1b" {
   }
 }
 
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vorx-vpc-prod.id
+
+  tags = {
+    Name = "igw-prod-vorx-vpc"
+  }
+}
 
 
 ## OUTPUTS DO NOSSO TF ##
